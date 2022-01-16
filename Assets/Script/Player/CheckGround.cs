8d1045dafba1b76e3657d91ca2d2e967047df426
ApuTHC <83385274+ApuTHC@users.noreply.github.com>
2022-01-16 13:54:52 -0500

    Traps 3

--- Assets/Script/Player/CheckGround.cs
+++ Assets/Script/Player/CheckGround.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CheckGround : MonoBehaviour
 {
-	private PlayerController _player;
+    private PlayerController _player;
     private Rigidbody2D _rb2d;
 
-	[SerializeField]
-	private float delayPlatFall = 0.5f;
+    [SerializeField]
+    private float delayPlatFall = 0.5f;
 
     void Start()
     {
@@ -17,60 +17,60 @@ public class CheckGround : MonoBehaviour
     }
 
 
-	void OnCollisionEnter2D(Collision2D col)
-	{
-		if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Head")
-		{
-			_player.SetGround(true);
-		}
-		if (col.gameObject.tag == "Platform")
-		{
-			_rb2d.velocity = Vector3.zero;
-			_player.transform.parent = col.transform;
-			_player.SetGround(true);
-		}
-		if (col.gameObject.tag == "PlatFall")
-		{
-			_player.SetGround(true);
-			Invoke("Fall", delayPlatFall);
-		}
-		if (col.gameObject.tag == "EndPoint")
-		{
-			_player.SetGround(true);
-			//Invoke("Win", 1.5f);
-		}
-	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Head" || col.gameObject.tag == "PlatStatic")
+        {
+            _player.SetGround(true);
+        }
+        if (col.gameObject.tag == "Platform")
+        {
+            _rb2d.velocity = Vector3.zero;
+            _player.transform.parent = col.transform;
+            _player.SetGround(true);
+        }
+        if (col.gameObject.tag == "PlatFall")
+        {
+            _player.SetGround(true);
+            Invoke("Fall", delayPlatFall);
+        }
+        if (col.gameObject.tag == "EndPoint")
+        {
+            _player.SetGround(true);
+            //Invoke("Win", 1.5f);
+        }
+    }
 
-	void Fall()
-	{
-		_player.SetGround(false);
-	}
+    void Fall()
+    {
+        _player.SetGround(false);
+    }
 
-	void OnCollisionExit2D(Collision2D col)
-	{
-		if (col.gameObject.tag == "Ground" || col.gameObject.tag == "EndPoint" )
-		{
-			_player.SetGround(false);
-		}
-		if (col.gameObject.tag == "Platform")
-		{
-			_player.transform.parent = null;
-			_player.SetGround(false);
-		}
-	}
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground" || col.gameObject.tag == "EndPoint" || col.gameObject.tag == "PlatStatic")
+        {
+            _player.SetGround(false);
+        }
+        if (col.gameObject.tag == "Platform")
+        {
+            _player.transform.parent = null;
+            _player.SetGround(false);
+        }
+    }
 
-	public void BoxJump(Vector3 _boxPos)
+    public void BoxJump(Vector3 _boxPos)
     {
         _player.BoxJump(_boxPos);
     }
 
-	public void Impulse(Vector2 vector)
-	{
-		_player.Impulse(vector);
-	}
+    public void Impulse(Vector2 vector)
+    {
+        _player.Impulse(vector);
+    }
 
-	public void EnemyKnockBack(Vector3 enemyPos)
-	{
-		_player.EnemyKnockBack(enemyPos);
-	}
+    public void EnemyKnockBack(Vector3 enemyPos)
+    {
+        _player.EnemyKnockBack(enemyPos);
+    }
 }
